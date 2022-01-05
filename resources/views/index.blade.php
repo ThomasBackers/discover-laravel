c610ff36a094a9467d9a78b2a49f880181e95336
ThomasBackers <thomasbackers@gmail.com>
2022-01-05 14:37:22 +0100

    a checkpoint in the middle of the homepage design

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,11 +29,11 @@
         Struggling to be a better web developer?
       </h2>
 
-      <p class="py-8 text-gray-500 text-l">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor!
+      <p class="py-8 text-gray-500 text-s">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, repellat!
       </p>
 
-      <p class="font-extrabold text-gray-600 text-l pb-9">
+      <p class="font-extrabold text-gray-600 text-s pb-9">
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
         Quibusdam aliquam velit nisi facilis soluta ipsam nulla, quaerat animi vero.
       </p>
@@ -44,5 +44,72 @@
     </div>
   </div>
 
-  <div></div>
+  <div class="text-center p-15 bg-black text-white">
+    <h2 class="text-2xl pb-5 text-l">
+      I'm an expert in...
+    </h2>
+
+    <span class="font-extrabold block text-4xl py-1">
+      Ux Design
+    </span>
+
+    <span class="font-extrabold block text-4xl py-1">
+      Project Management
+    </span>
+
+    <span class="font-extrabold block text-4xl py-1">
+      Digital Strategy
+    </span>
+
+    <span class="font-extrabold block text-4xl py-1">
+      Backend Development
+    </span>
+  </div>
+
+  <div class="text-center py-15">
+    <span class="uppercase text-s text-gray-400">
+      Blog
+    </span>
+
+    <h2 class="text-4xl font-bold py-10">
+      Recent Posts
+    </h2>
+
+    <p class="m-auto w-4/5 text-gray-500">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Saepe expedita atque odit, itaque eos quas rem, reiciendis
+      molestiae a ad perferendis.
+    </p>
+  </div>
+
+  <div class="sm:grid grid-cols-2 w-4/5 m-auto">
+    <div class="flex bg-yellow-700 text-gray-100 pt-10">
+      <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block">
+        <span class="uppercase text-xs">
+          PHP
+        </span>
+
+        <h3 class="text-xl font-bold py-10">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Doloribus esse, incidunt odit hic ex sapiente minima!
+          Quaerat similique fuga, ut ipsa ullam tempore veniam
+          nesciunt dolore aliquam obcaecati sed cumque.
+        </h3>
+
+        <a
+          href=""
+          class="uppercase bg-transparent border-2 border-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl"
+        >
+          Find Out More
+        </a>
+      </div>
+    </div>
+    <div>
+      <img 
+        src="https://cdn.pixabay.com/photo/2017/11/14/08/07/glasses-2947708_960_720.jpg"
+        alt=""
+        width="700"
+      >
+    </div>
+  </div>
 @endsection
