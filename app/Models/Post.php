93a2b93f67a5ba7e82b26de2795e882bb5377d8c
ThomasBackers <thomasbackers@gmail.com>
2022-01-07 15:42:46 +0100

    sluggable used into Post model to generate a slug from the post title

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,15 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
     use HasFactory;
+    use Sluggable;
 
     public function user()
     {
         // this goes hand in hand with the User model post function
         // one post belongs to one user
         return $this->belongsTo(User::class);
+    }
+
+    public function sluggable(): array //<- this means that the method must return an array
+    {
+        return [
+            'slug' => [
+                // it grabs the title and makes a slug outta it
+                'source' => 'title'
+            ]
+        ];
     }
 }
